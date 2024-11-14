571dcbcc4c19429f0a6ab4e092d600548460207b
Michelle Schofield <schofieldshelly05@gmail.com>
2024-11-14 23:46:47 +0000

    indicator buttons for carousel

---
+++ client/src/components/Indicators.tsx
@@ -0,0 +1,21 @@
+import { IndicatorDot } from './IndicatorDot';
+
+type Props = {
+  items: unknown[];
+  current: number;
+  onClick: (index: number) => void;
+};
+
+export function Indicators({ current, items, onClick }: Props) {
+  return (
+    <div className="flex justify-center flex-wrap">
+      {items.map((_, index) => (
+        <IndicatorDot
+          key={index}
+          onClick={() => onClick(index)}
+          active={current === index}
+        />
+      ))}
+    </div>
+  );
+}
--- client/src/pages/Flashcards.tsx
+++ client/src/pages/Flashcards.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { StudySet, FilledCard, readStudySet, readCards } from '../lib';
 import { FlippingCard } from '../components/FlippingCard';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { PokemonCard } from '../components/PokemonCard';
 import { BackOfCard } from '../components/BackOfCard';
+import { Indicators } from '../components/Indicators';
 
 export function Flashcards() {
   const [studySet, setStudySet] = useState<StudySet>();
@@ -34,7 +35,7 @@ export function Flashcards() {
   }, [studySetId]);
 
   function incrementIndex() {
-    if (!cards) throw new Error('cards dont exist');
+    if (!cards) throw new Error("cards don't exist");
     setIndex((index + 1) % cards.length);
     setIsFlipped(false);
   }
@@ -58,20 +59,31 @@ export function Flashcards() {
   return (
     <>
       <h2 className="text-3xl">{studySet.title}</h2>
-      <div className="flex justify-evenly items-center max-w-md">
-        <FaChevronLeft onClick={decrementIndex} />
-        <FlippingCard
-          isFlipped={isFlipped}
-          onFlip={() => setIsFlipped(!isFlipped)}
-          frontSide={
-            <PokemonCard
-              imageSrc={card.pokemonImageUrl}
-              caption={card.pokemonName}
-            />
-          }
-          backSide={<BackOfCard card={card} />}
+      <Link to="/flashcards">Change Study Set</Link>
+      <div className="max-w-96">
+        <div className="flex justify-evenly items-center max-w-md">
+          <FaChevronLeft
+            className="cursor-pointer rounded hover:bg-slate-400"
+            onClick={decrementIndex}
+          />
+          <FlippingCard
+            isFlipped={isFlipped}
+            onFlip={() => setIsFlipped(!isFlipped)}
+            frontSide={
+              <PokemonCard
+                imageSrc={card.pokemonImageUrl}
+                caption={card.pokemonName}
+              />
+            }
+            backSide={<BackOfCard card={card} />}
+          />
+          <FaChevronRight onClick={incrementIndex} />
+        </div>
+        <Indicators
+          items={cards}
+          current={index}
+          onClick={(index) => setIndex(index)}
         />
-        <FaChevronRight onClick={incrementIndex} />
       </div>
     </>
   );
