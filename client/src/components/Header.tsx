f24007b338b1765316891e32b436a627b5e4beee
Michelle Schofield <schofieldshelly05@gmail.com>
2024-11-20 18:55:56 +0000

    some header stuff

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -41,37 +41,37 @@ export function Header(): JSX.Element {
 
   return (
     <>
-      <div className="bg-blue-300 flex items-center justify-end px-2 h-16">
-        <Menu menuItems={menuItems} />
-        <div className="w-2/3 sm:w-2/5 md:w-1/4 xl:w-1/5">
-          <img
-            className="w-full object-contain"
-            src="https://fontmeme.com/permalink/241004/6e99ef9578d90391496b5f4b4459f196.png"
-          />
-        </div>
-        {user && (
-          <p
-            className="grow text-end underline underline-offset-2"
-            onClick={() => setModalOpen(true)}>
-            {user.username}
-          </p>
-        )}
-        <Modal onClose={() => setModalOpen(false)} isOpen={modalOpen}>
-          <div className="p-2 rounded">
-            <p>Signed in as {user?.username}</p>
-            <RedButton
-              onClick={() => {
-                setModalOpen(false);
-                handleSignOut();
-                navigate('/');
-              }}>
-              Sign out
-            </RedButton>
-            <Button onClick={() => setModalOpen(false)}>Close</Button>
-          </div>
-        </Modal>
-      </div>
       <div className="bg-slate-200 h-full">
+        <div className="bg-blue-300 flex items-center justify-end px-2 h-16 container m-auto border-x-2 border-blue-400">
+          {user && <Menu menuItems={menuItems} />}
+          <div className="w-2/3 sm:w-2/5 md:w-1/3 xl:w-1/5">
+            <img
+              className="w-full object-contain"
+              src="https://fontmeme.com/permalink/241004/6e99ef9578d90391496b5f4b4459f196.png"
+            />
+          </div>
+          {user && (
+            <p
+              className="grow text-end underline underline-offset-2 cursor-pointer"
+              onClick={() => setModalOpen(true)}>
+              {user.username}
+            </p>
+          )}
+          <Modal onClose={() => setModalOpen(false)} isOpen={modalOpen}>
+            <div className="p-2 rounded">
+              <p>Signed in as {user?.username}</p>
+              <RedButton
+                onClick={() => {
+                  setModalOpen(false);
+                  handleSignOut();
+                  navigate('/');
+                }}>
+                Sign out
+              </RedButton>
+              <Button onClick={() => setModalOpen(false)}>Close</Button>
+            </div>
+          </Modal>
+        </div>
         <div className="container px-2 m-auto bg-white border-x-2 border-slate-300 h-full">
           <Outlet />
         </div>
